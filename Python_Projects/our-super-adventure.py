d9b10cef67314e627b890c28efa0713b1691811a
Jonathan Thornton <thorntonj1@xavier.edu>
2019-10-07 09:05:55 -0400

    testing soup on osa script

--- Python_Projects/our-super-adventure.py
+++ Python_Projects/our-super-adventure.py
@@ -1,8 +1,11 @@
 
 #Goal: Create a script that automatically downloads the most recent Our Super Adventure comic from a static html page and save it to a local directory with the incremented number and name of the comic.
 
+#Comic is deployed on Monday so the bot should run on that day.
+
 #Created by Jon Thornton on 10/6/2019
 
+from bs4 import BeautifulSoup
 import shutil
 import requests
 import re
@@ -10,30 +13,32 @@ import re
 #Calls the website to pulldown HTML content
 # page = requests.get('https://www.oursuperadventure.com/')
 # content = page.text
+# soup = BeautifulSoup(content, 'html.parser')
 
-# with open("osa.html", "w") as Html_file:
-#     Html_file.write(content)
-# Html_file.close()
-# del page
+# print(content)
+# soup = BeautifulSoup('[<div id="comic"> <img src="https://www.oursuperadventure.com/wp-content/uploads/2019/09/OSA3-036-Sleepyhead.jpg" alt="Sleepyhead (30th September, 2019)" title="Sleepyhead (30th September, 2019)">	</div>]')
 
-# Test Case from the attributes in the meta tags. All the content is at the top of the html doc. 
-content = '<div id="comic">\n<img src="https://www.oursuperadventure.com/wp-content/uploads/2019/09/OSA3-036-Sleepyhead.jpg" alt="Sleepyhead (30th September, 2019)" title="Sleepyhead (30th September, 2019)"/>\n </div>'
+foo = soup.find_all(id="comic")
+tag = soup.img
+print(tag)
+# for line in foo:
+    # print(line)
 
-try:
-    #Meta tags contains the formal comic name and innumerated comic number.
-    div_block = re.search('<div id="comic">\n(.*)\n</div>', content).group(1)
+# print(soup)
+#     #Meta tags contains the formal comic name and innumerated comic number.
+#     div_block = re.search('<div id="comic">\n(.*)\n</div>', content).group(1)
 
-    # img_name = re.search('<meta property="og:title" content="(.+?)">', content).group(1) + '_2x.png'
-    # comic_num = re.search('<meta property="og:url" content="https://xkcd.com/(.+?)/">', content).group(1)
+#     # img_name = re.search('<meta property="og:title" content="(.+?)">', content).group(1) + '_2x.png'
+#     # comic_num = re.search('<meta property="og:url" content="https://xkcd.com/(.+?)/">', content).group(1)
 
-    # #Finds high res link off HTML call
-    # img_link = 'https://imgs.xkcd.com/comics/' + re.search('<meta property="og:image" content="https://imgs.xkcd.com/comics/(.+?)_2x.png">', content).group(1) + '_2x.png'
+#     # #Finds high res link off HTML call
+#     # img_link = 'https://imgs.xkcd.com/comics/' + re.search('<meta property="og:image" content="https://imgs.xkcd.com/comics/(.+?)_2x.png">', content).group(1) + '_2x.png'
 
-except AttributeError:
-    print('Failed to load in the data')
+# except AttributeError:
+#     print('Failed to load in the data')
 
 
-print(div_block)
+# print(div_block)
 
 #Copied code from xkcd_bot.py
 
