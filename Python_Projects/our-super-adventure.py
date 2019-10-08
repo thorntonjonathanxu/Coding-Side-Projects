e013fc6a27d1fccbac928a54913ae8970c240081
Jon <thorntonjonathanxu@gmail.com>
2019-10-07 21:47:55 -0400

    Simplified Date Parameteres for OSA

--- Python_Projects/our-super-adventure.py
+++ Python_Projects/our-super-adventure.py
@@ -15,41 +15,18 @@ page = requests.get('https://www.oursuperadventure.com/')
 content = page.text
 soup = BeautifulSoup(content, 'html.parser')
 
-print()
+#Pull out comic information from div with Id="comic"
+comic_tag = soup.find('div', { 'id' : 'comic' }).find('img', recursive=False)
+comic_name = comic_tag.get('title').split(' ')[0]
+img_src = comic_tag.get('src')
+date = soup.find('span',{'class' : 'post-date'}).string #Date is in a span class
 
-raw_tag = soup.find("div", { "id" : "comic" }).find("img", recursive=False)
-title_tag = raw_tag.get('title')
-img_src = raw_tag.get('src')
-
-#Test Case to pull down text for string manipulation
-# title_tag = 'Sleepyhead (30th September, 2019)'
-# img_src = 'https://www.oursuperadventure.com/wp-content/uploads/2019/09/OSA3-036-Sleepyhead.jpg'
-
-#Test Case from October 7th, 2019 with different formatting
-title_tag = 'Compliments (October 7th, 2019)'
-img_src = 'https://www.oursuperadventure.com/wp-content/uploads/2019/10/OSA3-037-Compliments.jpg'
-
-# print (raw_tag)
-print(title_tag)
-print(img_src)
-
-#Need to pull date out of the 'title' of the comic
-#Use string manipulation to find the values between the parenthesesis
-comic_name = title_tag.split('(')[0][:-1]
-date = title_tag.split('(')[1][:-1]
-date = date.split(' ')
-day = date[1][:-3]
-month = date[0]
-year = date[2]
 #Use datetime library to convert string date to YYYY-MM-dd format
-date = '{0}-{1}-{2}'.format(date[0],date[1][:-3],date[2])
-
-d = datetime.strptime(date, '%B-%d-%Y')
+d = datetime.strptime(date, '%B %d, %Y')
 date = d.strftime('%Y-%m-%d')
 
-
 #Throws a request to pull down the image
-print('Requesting image')
+print('Requesting Image')
 response = requests.get(img_src, stream=True)
 
 #Saves the file to a local directory with the name of the file.
